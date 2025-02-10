1f8a07c12ebb8893897d9e7dea20594eed6d8182
jared-karpiak <jkarpiak@nait.ca>
2025-02-10 14:48:02 -0700

    Add loop notes and examples
    Added more information about for, while, and do-while loops as well as examples.

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -80,14 +80,105 @@
             int secondNumber = int.Parse(Console.ReadLine());
 
             // Pre-test loops
+            // Pre-test loops will check if a condition is true BEFORE executing the code inside of the loop
+            // FOR loops and While loops are examples of Pre-test loops
 
-            // FOR LOOPS
+            // FOR LOOPS /////////////
+            // For loops are used to execute a block of code a specific number of times.
+            // They require three parts.
+            // 1. The variable that will be used as the counter
+            // 2. The logical condition that will break the loop
+            // 3. The operation to perform on the variable being used as the counter.
 
-            // WHILE LOOPS
+            //     (1)       (2)   (3)
+            //      |         |     |
+            //      |         |     |
+            //      V         V     V
+            for (int i = 0; i < 3; i++)
+            {
+                // This will write three times, each time it executes, the value of i will increase by 1
+                Console.WriteLine($"The value of i is {i}.");
+            }
+
+            // More often than not, a constant is not used in the conditional statement.
+            // It is more common to see a variable used as part of it.
+
+            int loopCount = 5;
+            for (int i = 0; i < loopCount; i++)
+            {
+                Console.WriteLine($"The value of i is {i}.");
+            }
+
+
+            // WHILE LOOPS ///////////
+            // A while loop will repeat the code inside of the loop when a certain condition evaluates to true.
+
+            // Using the following while loop:
+            // - if firstNum = 5 and secondNumber = 3, this loop will not execute at all,
+            //   because (firstNum < secondNumber) evaluates to false.
+            // - if firstNum = 3 and secondNumber = 5, then the loop will execute,
+            //   because (firstNum < secondNumber) evaluates to true.
+
+            while (firstNum < secondNumber)
+            {
+                // This will execute until (firstNum < secondNumber) evaluates to false.
+                // In this scenario, we must increase the value of firstNum or else we will have an infinite loop
+                Console.WriteLine($"The value of the first number is {firstNum++}.");
+
+                // We could also decrease secondNumber to prevent an infinite loop.
+                Console.WriteLine($"The value of the second number is {secondNumber--}.");
+            }
+
+            // While loops can easily slip into an infinite loop if care is not taken to make sure the logical
+            // condition is met.
+
 
             // Post-test loops
+            // Post-test loops will check if a condition is true AFTER the code inside of the loop has been executed.
+            // This guarantees that each block of code inside of a loop is executed at least once.
+            // In C#, this is mostly done using do-while loops
 
             // DO WHILE Loops
+
+            // Do while loops will always execute the code at least once. After the code is run, the logical check is
+            // performed which determines if we break out of the loop.
+            
+            // The following example will present a menu of options to a user that will reprint until
+            // the character a user enters is 'q'.
+
+            // First we must declare the variable "letter" outside of the do-while loop so it can be used inside
+            // of our logical condition. If it is declared inside of the loop, it will be out of scope and inaccessible to
+            // the logical condition.
+
+            char letter;
+            do
+            {
+                Console.WriteLine("""
+                    Give me an action:
+                    [a] = Write 'awesome'
+                    [b] = Write 'Boedica'
+                    [c] = Write 'cool'
+                    [q] = Quit program
+                    """);
+                Console.Write("Enter a letter: ");
+                letter = char.Parse(Console.ReadLine());
+
+                switch (letter)
+                {
+                    case 'a':
+                        Console.WriteLine("Awesome!");
+                        break;
+                    case 'b':
+                        Console.WriteLine("Boedica is a British Hero who rebelled against the Romans.");
+                        break;
+                    case 'c':
+                        Console.WriteLine("Cool!");
+                        break;
+                    default:
+                        Console.WriteLine($"You entered {letter}.");
+                        break;
+                }
+            } while (letter != 'q');
         }
     }
 }
